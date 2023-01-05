7d092a97a8016e30d8cb7f258e078fc8f3c492e5
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-05 08:10:55 +0530

    Create AddressContacts.cs

---
+++ AddressContacts.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooks
+{
+    public class AddressContacts
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+        public AddressContacts(string FirstName, string LastName, string Address, string City, string State, string Zip, string PhoneNumber, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.Zip = Zip;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+        }
+    }
+}
+
+    
