6ab11edd0b021f3dae5e77f1fcd79ddbab3afcb1
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-05 10:27:36 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,13 +2,22 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program:");
+            Console.WriteLine("Welcome to Address Book");
 
-            
+            int choice = 0;
+            while (choice != 2)
+            {
+                Console.WriteLine("1.Perform Operations \n2.Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice != 2)
+                    Book.AddPerson();
+            }
+            Book.ListPeople();
+            Console.WriteLine("Program Ends: Address Book: Press Enter");
+            Console.ReadLine();
+
         }
-
     }
-
 }
