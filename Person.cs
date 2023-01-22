56efaaf2a916eb54addf5b8404fc62577290440b
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-22 21:30:35 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookNext
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string PhoneNumber;
+        public string Address;
+        public string City;
+        public string State;
+        public string ZipCode;
+        public string PhoneNum;
+        public string EmailId;
+        public Person(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNum, string EmailId)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.ZipCode = ZipCode;
+            this.PhoneNum = PhoneNum;
+            this.EmailId = EmailId;
+        }
+
+    }
+}
