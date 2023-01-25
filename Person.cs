57099b64df0e2350f474dbf63c921248ce4bb62b
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-25 11:36:18 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookNext
+{
+    public class Person
+    {
+        public string[] Array_of_Details = new string[8];
+        public Person(string First, string Last, string Add, string City, string State, string Zip, string Phone, string Email)
+        {
+            Array_of_Details[0] = First;
+            Array_of_Details[1] = Last;
+            Array_of_Details[2] = Add;
+            Array_of_Details[3] = City;
+            Array_of_Details[4] = State;
+            Array_of_Details[5] = Zip;
+            Array_of_Details[6] = Phone;
+            Array_of_Details[7] = Email;
+        }
+        public void Check()
+        {
+            Console.WriteLine("\nThe details for {0} {1} are:\nAddress: {2}\nCity: {3}\nState: {4}\n" +
+                "Zip Code: {5}\nPhone Number: {6}\nEmail: {7}\n", Array_of_Details[0],
+                Array_of_Details[1], Array_of_Details[2], Array_of_Details[3],
+                Array_of_Details[4], Array_of_Details[5], Array_of_Details[6], Array_of_Details[7]);
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,14 +1,80 @@
-﻿namespace AddressBooks
+﻿using CollectionAddressBook;
+
+namespace AddressBookNext
 {
-    public class Program
+    class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program:");
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBooks Relatives = new AddressBooks();
+            AddressBooks Work = new AddressBooks();
+            int Control;
+            do
+            {
+                Console.WriteLine("\nChoose an Address Book: ");
+                Console.WriteLine("1 for Relatives Address book");
+                Console.WriteLine("2 for Work Address Book");
+                Console.WriteLine("3 to Search across all Address books");
+                Console.WriteLine("4 to View contacts in a City or State");
+                Console.WriteLine("0 to EXIT");
+                Control = Convert.ToInt32(Console.ReadLine());
+                switch (Control)
+                {
+                    case 1:
+                        Relatives.Access_to_Addressbook();
+                        break;
+                    case 2:
+                        Work.Access_to_Addressbook();
+                        break;
+                    case 3:
+                        Console.Write("Search by (City/State): ");
+                        string cityOrState = Console.ReadLine().ToLower();
+                        string name = "";
+                        int num = 0;
+                        if (cityOrState == "city")
+                        {
+                            Console.Write("Enter the name of the city: ");
+                            name = Console.ReadLine();
+                            num = 1;
+                        }
+                        else if (cityOrState == "state")
+                        {
+                            Console.Write("Enter the name of the state: ");
+                            name = Console.ReadLine();
+                            num = 2;
+                        }
+                        Console.WriteLine("Names of people living in {0} are:\n", name);
+                        int result = Relatives.search(name, num) + Work.search(name, num);
 
-            
+                        Console.WriteLine("Number of people in {0} are {1}", name, num);
+                        break;
+                    case 4:
+                        Console.WriteLine("\n1 for Relatives");
+                        Console.WriteLine("2 for Work");
+                        Console.WriteLine("0 to Exit");
+                        Console.Write("Pick an address book: ");
+                        int book = Convert.ToInt32(Console.ReadLine());
+                        switch (book)
+                        {
+                            case 1:
+                                Relatives.viewContacts();
+                                break;
+                            case 2:
+                                Work.viewContacts();
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+                }
+            }
+            while (Control != 0);
         }
-
     }
-
 }
