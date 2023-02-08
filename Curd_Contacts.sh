370bf8b0b60d22411ad3f4852ef57ddb9869ff02
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-08 16:01:32 +0530

    udated

---
+++ Curd_Contacts.sh
@@ -0,0 +1,24 @@
+Dell@DESKTOP-20QU9UV MINGW64 /d/BridgeLabz/RegularBatch/VS_Code(programs)/AddressBookServerJson
+$ json-server --port 4000 --routes CURD_routes.json --watch Relatives.json
+
+  \{^_^}/ hi!
+
+  Loading Relatives.json
+  Loading CURD_routes.json
+  Done
+
+  Resources
+  http://localhost:4000/Contacts
+
+  Other routes
+  /Contacts/list -> /Contacts
+  /Contacts/get/:id -> /Contacts/:id
+  /Contacts/create -> /Contacts
+  /Contacts/update/:id -> /Contacts/:id
+  /Contacts/delete/:id -> /Contacts/:id
+
+  Home
+  http://localhost:4000
+
+  Type s + enter at any time to create a snapshot of the database
+  Watching...
