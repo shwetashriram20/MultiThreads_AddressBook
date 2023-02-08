225d704d2e2ed9fdaae805e9df2b9eac58c89c72
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-08 16:02:50 +0530

    commnds

---
+++ GitCommand.sh
@@ -0,0 +1,17 @@
+$ json-server --watch Relatives.json
+
+  \{}/ hi!
+
+  Loading Relatives.json
+  Done
+
+  Resources
+  http://localhost:3000/Contacts
+
+  Home
+  http://localhost:3000
+
+  Type s + enter at any time to create a snapshot of the database
+  Watching...
+
+GET /Contacts 200 31.450 ms - 618
